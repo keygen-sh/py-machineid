14e376b0370f909388deee5ac33c08f1a0a58ef1
Juan Pedro Bretti Mandarano <juanbretti@users.noreply.github.com>
2023-01-27 18:43:54 +0100

    Add an id for Docker containers
    Usual Linux images for Docker containers, does not have the originally listed files.
    Following the conversation at https://github.com/denisbrodbeck/machineid/issues/10, I am including the following:
    Because `/proc/self/cgroup` seams to not be working on same Docker versions, I am also including `/proc/self/mountinfo`.
    
    Tested on `Python 3.11` running on `Debian GNU/Linux 11 (bullseye)` inside a Docker.

--- machineid/__init__.py
+++ machineid/__init__.py
@@ -74,7 +74,11 @@ def id() -> str:
     id = __read__('/var/lib/dbus/machine-id')
     if not id:
       id = __read__('/etc/machine-id')
-
+    if not id:
+      id = __exec__('head -1 /proc/self/cgroup|cut -d/ -f3')
+    if not id:
+      id = __exec__("grep 'systemd' /proc/self/mountinfo|cut -d/ -f3")
+    
   if platform.startswith('openbsd') or platform.startswith('freebsd'):
     id = __read__('/etc/hostid')
     if not id:
