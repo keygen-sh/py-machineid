7b12ce30376af8a190b7d31407c912821c009f94
Juan Pedro Bretti Mandarano <juanbretti@users.noreply.github.com>
2023-01-30 23:18:44 +0100

    Included "if 'docker' in mountinfo" for "linux"
    Following your review, I made some updates on the patch.

--- machineid/__init__.py
+++ machineid/__init__.py
@@ -75,10 +75,14 @@ def id() -> str:
     if not id:
       id = __read__('/etc/machine-id')
     if not id:
-      id = __exec__('head -1 /proc/self/cgroup|cut -d/ -f3')
+        cgroup = __read__('/proc/self/cgroup')
+        if 'docker' in cgroup:
+            id = __exec__('head -1 /proc/self/cgroup | cut -d/ -f3')
     if not id:
-      id = __exec__("grep 'systemd' /proc/self/mountinfo|cut -d/ -f3")
-    
+        mountinfo = __read__('/proc/self/mountinfo')
+        if 'docker' in mountinfo:
+            id = __exec__("grep 'systemd' /proc/self/mountinfo | cut -d/ -f3")
+
   if platform.startswith('openbsd') or platform.startswith('freebsd'):
     id = __read__('/etc/hostid')
     if not id:
