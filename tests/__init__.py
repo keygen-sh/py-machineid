28c6595ca7b3c0dce9375000d70884090e047e07
Zeke Gabrielse <ezekg@yahoo.com>
2023-10-10 21:49:02 -0500

    update tests to assert determinism

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,11 +9,16 @@ class TestMachineId(unittest.TestCase):
     self.assertGreater(len(machineid.hashed_id()), 0)
     self.assertNotEqual(machineid.hashed_id('foo'), machineid.hashed_id('bar'))
     self.assertNotEqual(machineid.hashed_id('foo'), machineid.hashed_id())
+    self.assertNotEqual(machineid.hashed_id('foo'), machineid.id())
+    self.assertNotEqual(machineid.hashed_id(), machineid.id())
+    self.assertEqual(machineid.hashed_id('foo'), machineid.hashed_id('foo'))
+    self.assertEqual(machineid.hashed_id(), machineid.hashed_id())
 
   def test_id(self):
     self.assertIsInstance(machineid.id(), str)
     self.assertGreater(len(machineid.id()), 0)
     self.assertNotEqual(machineid.id(), machineid.hashed_id())
+    self.assertEqual(machineid.id(), machineid.id())
 
 if __name__ == '__main__':
   unittest.main()
