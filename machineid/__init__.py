392fb36fc045200f8a12612e454215c8d6623b61
Zeke Gabrielse <ezekg@yahoo.com>
2024-07-04 09:41:59 -0500

    bump version

--- machineid/__init__.py
+++ machineid/__init__.py
@@ -22,7 +22,7 @@ Special thanks to Denis Brodbeck for his Go package, machineid (https://github.c
 :license: MIT, see LICENSE for more details.
 """
 
-__version__ = '0.5.1'
+__version__ = '0.6.0'
 __author__  = 'Zeke Gabrielse'
 __credits__ = 'https://github.com/denisbrodbeck/machineid'
 
