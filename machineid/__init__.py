5ce6daab92ef11f9ef9a524f71face8212b96362
A.Shpak <aleksander.shpak.als@gmail.com>
2024-12-14 23:30:01 +0300

    add alias to winregistry lib import

--- machineid/__init__.py
+++ machineid/__init__.py
@@ -34,9 +34,9 @@ from platform import uname
 from sys import platform
 
 try:
-  import winregistry
+  import winregistry as winregistry_lib
 except ImportError:
-  winregistry = None
+  winregistry_lib = None
 
 class MachineIdNotFound(RuntimeError):
   """
@@ -65,10 +65,10 @@ def __read__(path: str) -> str:
     return None
 
 def __reg__(key_name: str, value_name: str) -> str:
-  if winregistry is None:
+  if winregistry_lib is None:
     return None
   try:
-    with winregistry.open_value(key_name, value_name) as reg:
+    with winregistry_lib.open_value(key_name, value_name) as reg:
       if reg.data and isinstance(reg.data, str):
         return reg.data.strip()
   except OSError:
@@ -84,7 +84,7 @@ def id(winregistry: bool = True) -> str:
   if platform == 'darwin':
     id = __exec__("ioreg -d2 -c IOPlatformExpertDevice | awk -F\\\" '/IOPlatformUUID/{print $(NF-1)}'")
   elif platform in ('win32', 'cygwin', 'msys'):
-    if winregistry and winregistry is not None:
+    if winregistry:
       id = __reg__(r'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography', 'MachineGuid')
     else:
       id = __exec__("powershell.exe -ExecutionPolicy bypass -command (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID")
